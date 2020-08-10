152328b8ba880149ea9ba983a72cef2cc1e96a1f
Montu12345 <48189882+Montu12345@users.noreply.github.com>
2020-08-09 18:46:25 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,160 @@
+et backgroundColor, frogX, frogY, score, lives, gameIsOver, car1X, car1Y, car1V;
+
+function setup() {
+  // Canvas & color settings
+  createCanvas(500, 500);
+  colorMode(HSB, 360, 100, 100);
+  backgroundColor = 95;
+  frogX = width/2;
+  frogY = height - 20;
+  score = 0;
+  lives = 3;
+  gameIsOver = false;
+  car1X = 0;
+  car1Y = 100;
+  car1V = 5;
+  car2X = width;
+  car2Y = 400;
+  car2V = -5;
+  logX = 0;
+  logY = 300;
+  logV = 3;
+  car1Color = 0;
+  car2Color = 20;
+  car1ColorChange = 1;
+  car2ColorChange = -1;
+}
+
+function draw() {
+  background(backgroundColor);
+  // Code for gold goal line
+  fill(60, 80, 80);
+  rect(0, 0, width, 50);
+  // Code to display Frog
+  moveCars();
+  drawCars();
+  drawWater();
+  drawLog();
+  fill(120, 80, 80);
+  ellipse(frogX, frogY, 20);
+  moveLog();
+  checkCollisions();
+  checkWin();
+  displayScores();
+}
+
+function keyPressed() {
+  if (keyCode === UP_ARROW) {
+    frogY -= 10;
+  }
+  if (keyCode === LEFT_ARROW) {
+    frogX -= 10;
+  }
+  if (keyCode === RIGHT_ARROW) {
+    frogX += 10;
+  }
+  if (keyCode === DOWN_ARROW) {
+    frogY += 10;
+  }
+}
+
+function moveCars() {
+  car1X += car1V;
+  if (car1X > width){
+    car1X = 0 - car1Y;
+  }
+  car2X += car2V;
+  if (car2X < 0 - car2Y){
+    car2X = width;
+  }
+}
+
+function drawCars() {
+  // Code for car 1
+  fill(car1Color, 80, 80);
+  rect(car1X, car1Y, 40, 30);
+  fill(car2Color, 80, 80);
+  rect(car2X, car2Y, 40, 30);
+  // Code for additional cars
+}
+
+function drawWater() {
+  fill("cyan");
+  rect(0, 300, width, 50);
+}
+
+function checkCollisions() {
+  // If the frog collides with the car, reset the frog and subtract a life.
+  collideOne = collideRectCircle(car1X, car1Y, 40, 30, frogX, frogY, 20);
+  collideTwo = collideRectCircle(car2X, car2Y, 40, 30, frogX, frogY, 20);
+  collideLog = collideRectCircle(logX, logY, 100, 50, frogX, frogY, 20);
+  if (collideLog){
+    frogX += logV; 
+    if (frogX > width){
+      frogX = 0 - 50;
+      frogX += logV; 
+    }
+    collidesLog = true;
+  }
+  else{collidesLog = false;}
+  if (collideRectCircle(0, 300, width, 50, frogX, frogY, 20) && !collidesLog){
+    lives -=1;
+    frogX = width/2;
+    frogY = height - 20;
+  }
+  
+  if (collideOne){
+    lives -=1;
+    frogX = width/2;
+    frogY = height - 20;
+  }
+  if (collideTwo){
+    lives -=1;
+    frogX = width/2;
+    frogY = height - 20;
+  }
+  if (lives == 0){
+    gameIsOver = true;
+  }
+}
+
+function checkWin() {
+  // If the frog makes it into the yellow gold zone, increment the score
+  // and move the frog back down to the bottom.
+  if (frogY < 50){
+    score += 1;
+    frogX = width/2;
+    frogY = height - 20;
+  }
+}
+
+function displayScores() {
+  textSize(12);
+  fill(0);
+  // Display Lives
+  text(`Lives: ${lives}`, 10, 20);
+  
+  // Display Score
+  text(`Score: ${score}`, 10, 40);
+  // Display game over message if the game is over
+  if (gameIsOver){
+    backgroundColor = 0;
+    textSize(60);
+    textAlign(CENTER);
+    stroke(95);
+    text("Game Over", width/2, height/2);
+    textAlign(LEFT);
+  }
+}
+
+function drawLog(){
+  fill("brown");
+  rect(logX, logY, 100, 50);
+}
+
+function moveLog(){
+  logX += logV;
+  if (logX > width){
+    logX = 0 - logY;
+  }
+}
